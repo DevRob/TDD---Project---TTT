c636cfb400e0cdf74f8756727bc7911666eb37be
Robert Molnar <mail.robertmolnar@gmail.com>
2016-01-26 10:32:07 +0000

    Project setup

---
+++ js/spec/boardSpec.js
@@ -0,0 +1,7 @@
+describe('Board', function() {
+  it('is defined', function() {
+    var board = new Board();
+
+    expect(board).toBeDefined();
+  });
+});
---
+++ js/spec/guiSpec.js
@@ -0,0 +1,7 @@
+describe('GUI', function() {
+  it('is defined', function() {
+    var gui = new GUI();
+
+    expect(gui).toBeDefined();
+  });
+});
